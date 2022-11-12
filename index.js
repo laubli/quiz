19ffc3975ce374144f110cf6c8e03dce7d35d107
E21A359N <laureline.blivet@etu.univ-nantes.fr>
2022-11-12 19:52:24 +0100

    quiz debutant ok

--- index.js
+++ index.js
@@ -25,7 +25,12 @@ const showAccueil = () => {
     buttonStart.style.marginTop = screen.availHeight * 0.33 + "px";
     buttonRules.style.marginLeft = margeGauchebutton + "px";
     buttonRules.style.marginTop = screen.availHeight * 0.53 + "px";
-    console.log("consol" + buttonWidth);
+
+    if (tailleEcrant < 1000) {
+        margeGauchebutton = (tailleEcrant / 2) - 175;
+        buttonStart.style.marginLeft = margeGauchebutton + "px";
+        buttonRules.style.marginLeft = margeGauchebutton + "px";
+    }
 
     imageMillieu.appendChild(imageLogo);
     buttons.appendChild(buttonStart);
@@ -69,6 +74,7 @@ const home = () => {
 
 
     let tailleEcrant = screen.width;
+    console.log("tailleEcrant " + tailleEcrant);
     let tailleDiv = document.getElementById("particles-js").clientHeight;
     let heightImage = tailleDiv * 0.25;
     let widthImage = 469 * heightImage / 349;
@@ -76,13 +82,18 @@ const home = () => {
     let margeGauche = (tailleEcrant / 2) - (widthImage / 2);
     imageLogo.style.marginLeft = margeGauche + "px"
     //imageLogo.innerHTML = output;
-    console.log(imageLogo.style.marginLeft);
     buttonWidth = buttonStart.width;
     let margeGauchebutton = (tailleEcrant / 2) - (tailleEcrant / 4);
     buttonStart.style.marginLeft = margeGauchebutton + "px";
     buttonStart.style.marginTop = screen.availHeight * 0.33 + "px";
     buttonRules.style.marginLeft = margeGauchebutton + "px";
     buttonRules.style.marginTop = screen.availHeight * 0.53 + "px";
+
+    if (tailleEcrant < 1000) {
+        margeGauchebutton = (tailleEcrant / 2) - 175;
+        buttonStart.style.marginLeft = margeGauchebutton + "px";
+        buttonRules.style.marginLeft = margeGauchebutton + "px";
+    }
 
     buttons.appendChild(buttonStart);
     buttons.appendChild(buttonRules);
@@ -130,22 +141,61 @@ const resizepage = () => {
         let rep2 = document.getElementById("rep2");
         let rep3 = document.getElementById("rep3");
         let rep4 = document.getElementById("rep4");
+        let imageQuest = document.getElementById("imageAcc");
 
-        let margeGauchebutton = (tailleEcrant / 2) - (tailleEcrant / 4);
-        rep1.style.marginLeft = margeGauchebutton + "px";
-        rep2.style.marginLeft = margeGauchebutton + "px";
-        rep3.style.marginLeft = margeGauchebutton + "px";
-        rep4.style.marginLeft = margeGauchebutton + "px";
+        if (tailleEcrant < 1000) {
+            let margeGauchebutton = (tailleEcrant / 2) - 175;
+            rep1.style.marginLeft = margeGauchebutton + "px";
+            rep2.style.marginLeft = margeGauchebutton + "px";
+            rep3.style.marginLeft = margeGauchebutton + "px";
+            rep4.style.marginLeft = margeGauchebutton + "px";
 
-        rep1.style.marginTop = screen.availHeight * 0.30 + "px";
-        rep2.style.marginTop = screen.availHeight * 0.43 + "px";
-        rep3.style.marginTop = screen.availHeight * 0.56 + "px";
-        rep4.style.marginTop = screen.availHeight * 0.69 + "px";
+            rep1.style.marginTop = hauteurEcrant * 0.35 + "px";
+            rep2.style.marginTop = hauteurEcrant * 0.48 + "px";
+            rep3.style.marginTop = hauteurEcrant * 0.61 + "px";
+            rep4.style.marginTop = hauteurEcrant * 0.74 + "px";
+
+            imageQuest.style.height = window.innerHeight * 0.3 + "px";
+        } else {
+
+            let tailleButton = tailleEcrant * 0.4;
+            let margeGauchebutton = (tailleEcrant - tailleButton * 2) / 3;
+            rep1.style.width = "40%";
+            rep2.style.width = "40%";
+            rep3.style.width = "40%";
+            rep4.style.width = "40%";
+
+            rep1.style.marginLeft = margeGauchebutton + "px";
+            rep2.style.marginLeft = margeGauchebutton * 2 + tailleButton + "px";
+            rep3.style.marginLeft = margeGauchebutton + "px";
+            rep4.style.marginLeft = margeGauchebutton * 2 + tailleButton + "px";
+
+            rep1.style.marginTop = hauteurEcrant * 0.45 + "px";
+            rep2.style.marginTop = hauteurEcrant * 0.45 + "px";
+            rep3.style.marginTop = hauteurEcrant * 0.60 + "px";
+            rep4.style.marginTop = hauteurEcrant * 0.60 + "px";
+
+            imageQuest.style.height = window.innerHeight * 0.4 + "px";
+        }
+    } catch (error) { }
+
+    try {
+        if (tailleEcrant < 1000) {
+            let margeGauchebutton = (tailleEcrant / 2) - 175;
+            suivant = document.getElementById("suivant");
+            suivant.style.marginLeft = margeGauchebutton + "px";
+            suivant.style.marginTop = hauteurEcrant * 0.87 + "px";
+        } else {
+            suivant = document.getElementById("suivant");
+            suivant.style.marginTop = hauteurEcrant - (hauteurEcrant * 0.27) + "px";
+            suivant.style.marginLeft = tailleEcrant - 400 + "px";
+            suivant.style.width ="100px";
+        }
     } catch (error) { }
 
 }
 
-const resizeImage = () =>{
+const resizeImage = () => {
 
     let tailleEcrant = screen.availWidth;
     let hauteurEcrant = screen.height;
@@ -185,7 +235,7 @@ function start() {
     confirme.setAttribute("onclick", "startConfirme()");
     expert.setAttribute("onclick", "startExpert()");
 
-    let tailleEcrant = window.innerWidth;
+    let tailleEcrant = screen.availWidth;
     let margeGauchebutton = (tailleEcrant / 2) - (tailleEcrant / 4);
     debutant.style.marginLeft = margeGauchebutton + "px";
     confirme.style.marginLeft = margeGauchebutton + "px";
@@ -194,6 +244,13 @@ function start() {
     debutant.style.marginTop = screen.availHeight * 0.33 + "px";
     confirme.style.marginTop = screen.availHeight * 0.48 + "px";
     expert.style.marginTop = screen.availHeight * 0.63 + "px";
+
+    if (tailleEcrant < 1000) {
+        margeGauchebutton = (tailleEcrant / 2) - 175;
+        debutant.style.marginLeft = margeGauchebutton + "px";
+        confirme.style.marginLeft = margeGauchebutton + "px";
+        expert.style.marginLeft = margeGauchebutton + "px";
+    }
 
     buttons.appendChild(debutant);
     buttons.appendChild(confirme);
@@ -245,6 +302,17 @@ function startJeu(quest) {
     imageQuest.setAttribute("src", "./assets/img/debutant/q1.jpg");
     imageQuest.style.maxWidth = screen.width - 6 + "px";
 
+    
+    question = document.createElement("input");
+    question.setAttribute("id", "question");
+    question.setAttribute("value", quiz[0].question);
+    question.style.paddingTop = "0px";
+    question.style.paddingBottom = "0px";
+    question.style.border = "none";
+    question.style.width = "90%";
+    question.style.marginLeft = "5%";
+    question.disabled = true;
+
     rep1 = document.createElement("input");
     rep2 = document.createElement("input");
     rep3 = document.createElement("input");
@@ -270,17 +338,43 @@ function startJeu(quest) {
     rep4.setAttribute("onclick", "verifReponse(4)");
 
     let tailleEcrant = screen.width;
-    let margeGauchebutton = (tailleEcrant / 2) - (tailleEcrant / 4);
-    rep1.style.marginLeft = margeGauchebutton + "px";
-    rep2.style.marginLeft = margeGauchebutton + "px";
-    rep3.style.marginLeft = margeGauchebutton + "px";
-    rep4.style.marginLeft = margeGauchebutton + "px";
+    if (tailleEcrant < 1000) {
+        let margeGauchebutton = (tailleEcrant / 2) - 175;
+        rep1.style.marginLeft = margeGauchebutton + "px";
+        rep2.style.marginLeft = margeGauchebutton + "px";
+        rep3.style.marginLeft = margeGauchebutton + "px";
+        rep4.style.marginLeft = margeGauchebutton + "px";
 
-    rep1.style.marginTop = screen.availHeight * 0.30 + "px";
-    rep2.style.marginTop = screen.availHeight * 0.43 + "px";
-    rep3.style.marginTop = screen.availHeight * 0.56 + "px";
-    rep4.style.marginTop = screen.availHeight * 0.69 + "px";
+        question.style.marginTop = screen.availHeight * 0.25 + "px";
+        rep1.style.marginTop = screen.availHeight * 0.30 + "px";
+        rep2.style.marginTop = screen.availHeight * 0.43 + "px";
+        rep3.style.marginTop = screen.availHeight * 0.56 + "px";
+        rep4.style.marginTop = screen.availHeight * 0.69 + "px";
+    } else {
 
+        tailleEcrant = window.innerWidth;
+        let tailleButton = tailleEcrant * 0.4;
+        let margeGauchebutton = (tailleEcrant - tailleButton * 2) / 3;
+        rep1.style.width = "40%";
+        rep2.style.width = "40%";
+        rep3.style.width = "40%";
+        rep4.style.width = "40%";
+
+        rep1.style.marginLeft = margeGauchebutton + "px";
+        rep2.style.marginLeft = margeGauchebutton * 2 + tailleButton + "px";
+        rep3.style.marginLeft = margeGauchebutton + "px";
+        rep4.style.marginLeft = margeGauchebutton * 2 + tailleButton + "px";
+        
+        question.style.marginTop = screen.availHeight * 0.35 + "px";
+        rep1.style.marginTop = screen.availHeight * 0.45 + "px";
+        rep2.style.marginTop = screen.availHeight * 0.45 + "px";
+        rep3.style.marginTop = screen.availHeight * 0.60 + "px";
+        rep4.style.marginTop = screen.availHeight * 0.60 + "px";
+
+        imageQuest.style.height = window.innerHeight * 0.4 + "px";
+    }
+
+    buttons.appendChild(question);
     buttons.appendChild(rep1);
     buttons.appendChild(rep2);
     buttons.appendChild(rep3);
@@ -292,20 +386,35 @@ function startJeu(quest) {
 function questionSuivante() {
 
     let id = document.getElementById("numero").innerText;
+    id = id - 1 + 2;
+    document.getElementById("numero").innerText = id;
+
+    try {
+        suivant = document.getElementById("suivant");
+        buttons.removeChild(suivant);
+    } catch (error) { }
+
 
     rep1 = document.getElementById("rep1");
     rep2 = document.getElementById("rep2");
     rep3 = document.getElementById("rep3");
     rep4 = document.getElementById("rep4");
 
-    rep1.setAttribute("value", quiz[id].propositions[0]);
-    rep2.setAttribute("value", quiz[id].propositions[1]);
-    rep3.setAttribute("value", quiz[id].propositions[2]);
-    rep4.setAttribute("value", quiz[id].propositions[3]);
+    rep1.setAttribute("value", quiz[id - 1].propositions[0]);
+    rep2.setAttribute("value", quiz[id - 1].propositions[1]);
+    rep3.setAttribute("value", quiz[id - 1].propositions[2]);
+    rep4.setAttribute("value", quiz[id - 1].propositions[3]);
+
+    document.getElementById("question").setAttribute("value", quiz[id - 1].question);
+
+    rep1.disabled = false;
+    rep2.disabled = false;
+    rep3.disabled = false;
+    rep4.disabled = false;
 
     let imageQuest = document.getElementById("imageAcc");
     imageQuest.setAttribute("src", "./assets/img/" + niveau + "/q" + id + ".jpg");
-    
+
     resizeImage();
 
 }
@@ -316,13 +425,38 @@ function verifReponse(numRep) {
     idRep = "rep" + numRep;
     console.log(document.getElementById(idRep).value);
     console.log(quiz[id - 1].réponse);
-    if (quiz[id - 1].réponse === document.getElementById(idRep).value){
-        console.log("vrais");
+
+    suivant = document.createElement("input");
+    suivant.setAttribute("id", "suivant");
+    suivant.setAttribute("value", "Suivant");
+    suivant.setAttribute("type", "button");
+    suivant.setAttribute("onclick", "questionSuivante()");
+    if (screen.availWidth < 1000) {
+        suivant.style.marginTop = screen.availHeight - (screen.availHeight * 0.18) + "px";
+        let margeGauchebutton = (screen.width / 2) - 175;
+        suivant.style.marginLeft = margeGauchebutton + "px";
+    } else {
+        suivant.style.marginTop = window.innerHeight - (window.innerHeight * 0.18) + "px";
+        suivant.style.marginLeft = window.innerWidth - 400 + "px";
+        suivant.style.width ="100px";
     }
 
-    id = id - 1 + 2;
-    document.getElementById("numero").innerText = id;
 
+
+    suivant.style.marginRight = "0px";
+    buttons.appendChild(suivant);
+
+    if (quiz[id - 1].réponse === document.getElementById(idRep).value) {
+        console.log("vrais");
+    }
+    rep1 = document.getElementById("rep1");
+    rep2 = document.getElementById("rep2");
+    rep3 = document.getElementById("rep3");
+    rep4 = document.getElementById("rep4");
+    rep1.disabled = true;
+    rep2.disabled = true;
+    rep3.disabled = true;
+    rep4.disabled = true;
 }
 
 
